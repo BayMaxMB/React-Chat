885852d51ac3ad21aceb545e18904c57dc5b3473
Begzod Muxamedov <bmuxamedov585@bk.ru>
2019-09-14 22:13:35 +0500

    fix: make secure connection

--- src/Chat.js
+++ src/Chat.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import ChatInput from './ChatInput'
 import ChatMessage from './ChatMessage'
 
-const URL = 'ws://st-chat.shas.tel'
+const URL = 'wss://wssproxy.herokuapp.com/'
 
 class Chat extends Component {
   state = {
-    name: '',
+    name: 'BM',
     messages: [],
   }
 
